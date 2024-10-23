c9472f16d3b2f29a5fc6cd678219ffee6f8cba2f
matthias-devfr <matthiasgdt@gmail.com>
2024-10-23 17:10:43 +0200

    Update ordinateur_client.sh

--- ordinateur_client.sh
+++ ordinateur_client.sh
@@ -11,22 +11,22 @@
         # Demander les identifiants pour la machine cible
         # Se connecter 
         # Message erreur / succes
-        # Envoyer l'odre d'arrêt système
+        shutdown now
     #A2 Redémarrage
         # Demander les identifiants pour la machine cible
         # Se connecter 
         # Message erreur / succes
-        # Envoyer l'ordre de redémarrage système
+        reboot
     #A3 Verrouillage
         # Demander les identifiants pour la machine cible
         # Se connecter 
         # Message erreur / succes
-        # Envoyer l'ordre de redémarrage système
+        logout
     #A4 Mise-à-jour du système
         # Demander les identifiants pour la machine cible
         # Se connecter 
         # Message erreur / succes
-        # Commande sudo apt update upgrade 
+        sudo apt update && sudo apt upgrade -y
         # Msg erreur/réussie
         # Se déconnecter
     #A5 Création de répertoire
@@ -35,14 +35,15 @@
         # Message erreur / succes
         # Demander le chemin de création du répertoire
         # Confirmation
-        # Création et cd dans le dossier 
+        mkdir $... && cd $...
         # Msg erreur/réussi
     #A6 Suppression de répertoire
         # Demander les identifiants pour la machine cible
         # Se connecter 
         # Message erreur / succes
         # Demander le chemin du répertoire à supprimer
-        # Confirmation 
+        # Confirmation
+        rm -r $...
         # Msg erreur/réussi
     #A7 Prise de main à distance (GUI) ???
         # Demander les identifiants pour la machine cible
@@ -52,24 +53,25 @@
         # Demander les identifiants pour la machine cible
         # Se connecter 
         # Message erreur / succes
-        # Commande avec iptables ? ufw ? 
+        # Commande avec iptables ? ufw ?
     #A9 Activation du pare-feu
         # Demander les identifiants pour la machine cible
         # Se connecter 
         # Message erreur / succes
-        # Commande avec iptables ? ufw ? 
+        sudo ufw enable
+        iptable ??
     #A10 Désactivation du pare-feu
         # Demander les identifiants pour la machine cible
         # Se connecter 
         # Message erreur / succes
-        # Commande avec iptables ? ufw ? 
+        sudo ufw disable
     #A11 Installation de logiciel
         # Demander les identifiants pour la machine cible
         # Se connecter 
         # Message erreur / succes
         # Demander le nom du logiciel 
         # Confirmation ?
-        # Test installation 
+        sudo apt install $... -y
             # Si oui -> succès
             # Si non -> erreur 
     #A12 Désinstallation de logiciel
@@ -78,7 +80,7 @@
         # Message erreur / succes
         # Demander le nom du logiciel 
         # Confirmation 
-        # Test désinstallation 
+        sudo apt remove $... -y  
             #Si oui -> succès 
             #Si non -> erreur 
     #A13 Exécution de script sur la machine distante
@@ -96,23 +98,44 @@
         # Demander les identifiants pour la machine cible
         # Se connecter
         # Message erreur / succes
-        # Commande /etc/network/interfaces  grep expression + nbr de lignes
+        cat /etc/network/interfaces | grep enp*  
     #I3 Adresse IP de chaque interface
         # Demander les identifiants pour la machine cible
         # Se connecter
         # Message erreur / succes
-        #Commande grep /etc/network/interfaces
+        cat /etc/network/interfaces | grep
     #I4 Adresse Mac
         # Demander les identifiants pour la machine cible
         # Se connecter
         # Message erreur / succes
-        #Confirmer la machine ? 
-        # Grep le fichier des addresses MAC 
+        #Confirmer la machine ?
+        # commande
+        ip a | grep ether
     #I5 Liste des applications/paquets installées
+        # Demander les identifiants pour la machine cible
+        # Se connecter
+        apt list --installed 
+        # Message erreur / succes
     #I6 Liste des utilisateurs locaux
+        # Demander les identifiants pour la machine cible
+        # Se connecter
+        # Message erreur / succes
+        cat /etc/passwd | grep /home 
     #I7 Type de CPU, nombre de coeurs, etc.
+        # Demander les identifiants pour la machine cible
+        # Se connecter
+        # Message erreur / succes
+        cat /proc/cpuinfo
     #I8 Mémoire RAM totale
+        # Demander les identifiants pour la machine cible
+        # Se connecter
+        # Message erreur / succes
+        cat /proc/meminfo
     #I9 Utilisation de la RAM
+        # Demander les identifiants pour la machine cible
+        # Se connecter
+        # Message erreur / succes
+        cat /proc/meminfo | grep MemFree
     #I10 Utilisation du disque
     #I11 Utilisation du processeur
     #I12 Liste des ports ouverts
