aba71632b8760d136a95e5e051c566e611aeef31
AntoinePaulay <antoine.paulay@gmail.com>
2024-10-27 16:11:43 +0100

    Update powershell_ordiclient.ps1

--- powershell_ordiclient.ps1
+++ powershell_ordiclient.ps1
@@ -37,17 +37,50 @@ Uninstall-Package
 
 #Menu 2 Info 
     #I1 Version de l'OS
-    Get-ComputerInfo | Select-Object WindowsVersion, OSArchitecture
+Get-ComputerInfo | Select-Object WindowsVersion, OSArchitecture
+Get-WmiObject Win32_OperatingSystem | Select-Object Caption, Version, ServicePackMajorVersion, OSArchitecture, CSName, WindowsDirectory, NumberOfUsers, BootDevice
     
     #I2 Nombre d'interface
+
+Get-NetAdapter | fl Name, InterfaceIndex, MacAddress, MediaConnectionState, LinkSpeed
+
     #I3 Adresse IP de chaque interface
+
+Get-NetIPConfiguration
+
     #I4 Adresse Mac
+
+Get-NetAdapter | select ifIndex, Name, MacAddress
+
     #I5 Liste des applications/paquets installées
+
+Get-AppxPackage
+
     #I6 Liste des utilisateurs locaux
+    
+Get-LocalUser
+
     #I7 Type de CPU, nombre de coeurs, etc.
+
+Get-WmiObject Win32_Processor
+
     #I8 Mémoire RAM totale
+
+Get-CimInstance win32_physicalmemory | Format-Table Manufacturer,Banklabel,Configuredclockspeed,Devicelocator,Capacity,Serialnumber -autosize
+
     #I9 Utilisation de la RAM
+
+Get-WmiObject Win32_ComputerSystem | Select-Object -ExpandProperty TotalPhysicalMemory
+
     #I10 Utilisation du disque
+
+Get-PSDrive   Get-WmiObject Win32_LogicalDisk | Select SystemName,DeviceID,VolumeName,@{Name="size (Go)";Expression={"{0:N1}"-f($_.size/1GB)}},@{Name="freespace (Go)";Expression={"{0:N1}"-f($_.freespace/1GB)}} | Format-Table -GroupBy Name -AutoSize
+
     #I11 Utilisation du processeur
+
+Get-WmiObject Win32_Processor | Measure-Object -Property LoadPercentage -Average).Average
+
     #I12 Liste des ports ouverts
     #I13 Statut du pare-feu
+
+Get-NetFirewallProfile
